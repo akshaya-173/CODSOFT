7c3c4d32ec4225cc7235ca87f59337e439004b9c
akshaya-173 <sakshayanadar@gmail.com>
2026-02-08 01:11:08 +0530

    Add files via upload

---
+++ Claculator Task 3/script.js
@@ -0,0 +1,36 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+let expression = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.innerText;
+
+        if (value === "C") {
+            expression = "";
+            display.value = "";
+        }
+        else if (value === "⌫") {
+            expression = expression.slice(0, -1);
+            display.value = expression;
+        }
+        else if (value === "=") {
+            try {
+                expression = eval(expression);
+                display.value = expression;
+            } catch {
+                display.value = "Error";
+                expression = "";
+            }
+        }
+        else if (value === "%") {
+            expression = expression / 100;
+            display.value = expression;
+        }
+        else {
+            expression += value;
+            display.value = expression;
+        }
+    });
+});
